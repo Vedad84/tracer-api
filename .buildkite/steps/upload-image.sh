637c0600d429e65fbf2904eab7193daa11625ac1
Ivan Loboda <lobodatell@yandex.ru>
2022-03-30 10:27:44 +0300

    fix upload script

--- .buildkite/steps/upload-image.sh
+++ .buildkite/steps/upload-image.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -euo pipefail
+set ${NEON_EVM_COMMIT:=ci-tracing-api-develop-v1.8.12}
+
+echo "Tracer API revision=${BUILDKITE_COMMIT}"
+echo "Neon EVM revision=${NEON_EVM_COMMIT}"
 
 docker images
 
 docker login -u=${DHUBU} -p=${DHUBP}
 
-docker-compose -f docker-compose-test.yml push tracer-db neon-tracer neon-tracer-test
+docker push neonlabsorg/tracer-db:${BUILDKITE_COMMIT}
+docker push neonlabsorg/neon-tracer:${BUILDKITE_COMMIT}
+docker push neonlabsorg/neon-tracer-test:${BUILDKITE_COMMIT}
