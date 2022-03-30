eae2a809dc8cca617d6461258c2f8db7edbcd67c
Ivan Loboda <lobodatell@yandex.ru>
2022-03-30 10:00:46 +0300

    edit build script

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
-set ${NEON_REVISION:=ci-tracing-api-1.8.12}
+set ${NEON_EVM_COMMIT:=ci-tracing-api-v0.5.1}
 
 echo "Tracer API revision=${BUILDKITE_COMMIT}"
-echo "Neon EVM revision=${NEON_REVISION}"
+echo "Neon EVM revision=${NEON_EVM_COMMIT}"
 
-docker-compose -f docker-compose-test.yml build tracer-db neon-tracer neon-tracer-test
+docker build -t neonlabsorg/tracer-db:${BUILDKITE_COMMIT} ./clickhouse
+docker build -t neonlabsorg/neon-tracer:${BUILDKITE_COMMIT} --build-arg NEON_REVISION=${NEON_EVM_COMMIT} .
+docker build -t neonlabsorg/neon-tracer-test:${BUILDKITE_COMMIT} ./tests
