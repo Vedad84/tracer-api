fc993e7a314badd66d7cb01cb3c258445a73998a
Ivan Loboda <loboda.ivan.y@gmail.com>
2022-07-28 22:11:32 +0400

    Fix docker-compose

--- start_tracer.sh
+++ start_tracer.sh
@@ -4,7 +4,8 @@ neon-cli --commitment confirmed --url $SOLANA_URL --evm_loader "$EVM_LOADER" neo
 export $(neon-cli --commitment confirmed --url $SOLANA_URL --evm_loader "$EVM_LOADER" neon-elf-params)
 
 neon-tracer -l $LISTEN_ADDR \
-      -c $TRACER_DB_URL \
+      -h $TRACER_DB_HOST \
+      -P $TRACER_DB_PORT \
       -d $TRACER_DB_NAME \
       -u $TRACER_DB_USER \
       -p $TRACER_DB_PASSWORD \
