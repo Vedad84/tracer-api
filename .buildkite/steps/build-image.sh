3625ece4baa8cc5eac4ead7f83ed5c378a2d10e8
Ivan Loboda <loboda.ivan.y@gmail.com>
2022-08-02 20:57:52 +0400

    deployment rework start

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 set -euo pipefail
-set ${NEON_EVM_COMMIT:=develop}
+set ${NEON_EVM_COMMIT:=latest}
+set ${SOLANA_IMAGE:=neonlabsorg/solana:v1.11.3-dumper-plugin}
 
 echo "Tracer API revision=${BUILDKITE_COMMIT}"
 echo "Neon EVM revision=${NEON_EVM_COMMIT}"
+echo "Solana: ${SOLANA_IMAGE}"
 
-docker build -t neonlabsorg/tracer-db:${BUILDKITE_COMMIT} ./clickhouse
+echo -e "\n\n\nBuilding Tracer-API..."
 docker build -t neonlabsorg/neon-tracer:${BUILDKITE_COMMIT} --build-arg NEON_REVISION=${NEON_EVM_COMMIT} .
+echo -e "\n\n\nBuilding Tests..."
 docker build -t neonlabsorg/neon-tracer-test:${BUILDKITE_COMMIT} ./tests
-docker build -t neonlabsorg/neon-rpc:${BUILDKITE_COMMIT} ./nginx
+echo -e "\n\n\nBuilding Neon-RPC..."
+docker build -t neonlabsorg/neon-rpc:${BUILDKITE_COMMIT} ./neon-rpc
+echo -e "\n\n\nBuilding Neon Dumper Plugin..."
+docker build -t neonlabsorg/neon-dumper-plugin:${BUILDKITE_COMMIT} ./neon-dumper-plugin
+echo -e "\n\n\nBuilding Neon Validator..."
+docker build \
+  -t neonlabsorg/neon-validator:${BUILDKITE_COMMIT} \
+  --build-arg NEON_EVM_COMMIT=${NEON_EVM_COMMIT} \
+  --build-arg SOLANA_IMAGE=${SOLANA_IMAGE} \
+  --build-arg ACCOUNT_DUMPER_IMAGE=neonlabsorg/neon-dumper-plugin:${BUILDKITE_COMMIT} \
+  ./neon-validator
+
 echo complete!
