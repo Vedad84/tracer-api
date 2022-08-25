5ba1aa87a32e1f5bf3c6ac9a8bc07a2d10dff4ab
Ivan Loboda <loboda.ivan.y@gmail.com>
2022-08-25 20:43:50 +0300

    use test solana validator

--- .buildkite/steps/revision.sh
+++ .buildkite/steps/revision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set ${NEON_EVM_REVISION:=latest}
-set ${SOLANA_REVISION:=v1.11.3}
+set ${SOLANA_REVISION:=736c4120ecad16dd70c4c8e7453579f4c79b5e25}
 
 echo "Tracer API revision=${BUILDKITE_COMMIT}"
 echo "Neon EVM revision=${NEON_EVM_REVISION}"
