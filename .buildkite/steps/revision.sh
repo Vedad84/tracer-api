5634ed7c24230de9dd0a46531389aa7ea3bf2a9e
sinev-valentine <sinev-valentine@yandex.ru>
2023-03-13 15:55:08 +0300

    fix

--- .buildkite/steps/revision.sh
+++ .buildkite/steps/revision.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set ${NEON_REVISION:=latest}
+set ${REVISION:=$BUILDKITE_COMMIT}
 
-echo "Tracer API revision=${BUILDKITE_COMMIT}"
+echo "Tracer API revision=${REVISION}"
 echo "Neon EVM revision=${NEON_REVISION}"
