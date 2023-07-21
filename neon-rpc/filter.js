144f4f72e300a1b0b26b5dd5a22371b20b5bc45b
Denis K <100715772+dk-neon@users.noreply.github.com>
2023-07-21 13:04:08 +0500

    Improved Neon RPC Nginx filter

--- neon-rpc/filter.js
+++ neon-rpc/filter.js
@@ -1,59 +1,45 @@
-function isTracingMethod(req) {
-    let method = req.method;
-    return method === "debug_traceBlockByNumber" ||
-        method === "debug_traceTransaction" ||
-        method === "debug_traceCall" ||
-        method === "trace_filter" ||
-        method === "trace_get" ||
-        method === "trace_transaction" ||
-        method === "trace_block" ||
-        method === "trace_call" ||
-        method === "trace_callMany" ||
-        method === "trace_rawTransaction" ||
-        method === "trace_replayTransaction" ||
-        method === "trace_replayBlockTransactions";
+const tracingMethods = {
+    'debug_traceBlockByHash': null,
+    'debug_traceBlockByNumber': null,
+    'debug_traceCall': null,
+    'debug_traceTransaction': null,
+    'trace_block': null,
+    'trace_call': null,
+    'trace_callMany': null,
+    'trace_filter': null,
+    'trace_get': null,
+    'trace_rawTransaction': null,
+    'trace_replayBlockTransactions': null,
+    'trace_replayTransaction': null,
+    'trace_transaction': null,
 }
 
-function tagIsPredefined(tag) {
-  return tag === 'latest' || tag === 'pending' || tag === 'earliest';
+const eip1898Methods = {
+    'eth_getStorageAt': 2,
+    'eth_getBalance': 1,
+    'eth_getCode': 1,
+    'eth_getTransactionCount': 1,
+    'eth_call': 1,
+}
+
+const predefinedTags = {
+    'latest': null,
+    'pending': null,
+    'earliest': null,
 }
 
 function isEIP1898Method(req) {
-    let method = req.method;
-    let params = req.params;
-
-    if (method === "eth_getStorageAt") {
-        return !tagIsPredefined(params[2]);
-    }
-    else if (method === "eth_getBalance") {
-        return !tagIsPredefined(params[1]);
-    }
-    else if (method === "eth_getCode") {
-        return !tagIsPredefined(params[1]);
-    }
-    else if (method === "eth_getTransactionCount") {
-        return !tagIsPredefined(params[1]);
-    }
-    else if (method === "eth_call") {
-        return !tagIsPredefined(params[1]);
-    }
-
-    return false;
+    let paramIndex = eip1898Methods[req.method];
+    return (paramIndex !== undefined) && predefinedTags[req.params[paramIndex]] === undefined;
 }
 
 async function process(req) {
     let json = JSON.parse(req.requestText);
-    if (isTracingMethod(json)) {
-        req.internalRedirect("/tracer");
-        return;
+    if (tracingMethods[json.method] !== undefined || isEIP1898Method(json)) {
+        req.internalRedirect('/tracer');
+    } else {
+        req.internalRedirect('/proxy');
     }
-
-    if (!isEIP1898Method(json)) {
-        req.internalRedirect("/proxy");
-        return;
-    }
-
-    req.internalRedirect("/tracer");
 }
 
 export default { process }
